acbe4d8155ba2ddd1aeb92a8a711323a563fe0e0
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-25 13:13:21 +0300

    Defined vriablebs required in the class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,12 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.update_collection(self)
